c6a01167af5a832dcfedc3652272bd1219b29128
Ben Hoskings <ben@hoskings.net>
2010-05-08 17:17:23 +1000

    Filled out the CreateRuns migration with initial stuff.

--- db/migrate/20100508065013_create_runs.rb
+++ db/migrate/20100508065013_create_runs.rb
@@ -1,6 +1,9 @@
 class CreateRuns < ActiveRecord::Migration
   def self.up
     create_table :runs do |t|
+      t.string :dep_name, :limit => 128, :null => false
+      t.string :source_url, :limit => 128, :null => false
+      t.string :result, :limit => 8, :null => false
 
       t.timestamps
     end
