e33b1d5805da72c495310476ed9fa3e406ba549c
Ben Hoskings <ben@hoskings.net>
2010-05-08 20:22:12 +1000

    Added deps#search.

--- app/controllers/deps_controller.rb
+++ app/controllers/deps_controller.rb
@@ -1,2 +1,10 @@
 class DepsController < ApplicationController
+  def search
+    @deps = Dep.search(params[:q])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @deps.map(&:info) }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 BabushkaMe::Application.routes.draw do |map|
   resources :sources
 
+  match '/deps/search.:format/:q', :to => 'deps#search'
   resources :deps
 
   resources :runs
