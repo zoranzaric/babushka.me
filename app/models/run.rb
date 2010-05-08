4f58904a4359794262f174614daa056f45964ee7
Ben Hoskings <ben@hoskings.net>
2010-05-08 17:17:42 +1000

    Some initial validations and scopes for Run.

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,2 +1,11 @@
 class Run < ActiveRecord::Base
+
+  validates_presence_of :dep_name, :source_url
+  validates_inclusion_of :result, :in => %w[ok fail error]
+
+  scope :succeeded, where(:result => 'ok')
+  scope :failed, where(:result => 'failed')
+  scope :crashed, where(:result => 'error')
+  scope :for_source, lambda {|url| where(:source_url => url) }
+
 end
