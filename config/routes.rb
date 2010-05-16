618bf8a6ca4c84da2e973078592641888e068b41
Ben Hoskings <ben@hoskings.net>
2010-05-16 23:34:30 +1000

    Added /up action to render the bootstrap script.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BabushkaMe::Application.routes.draw do |map|
   match '/deps/search.:format/:q', :to => 'deps#search', :conditions => {:method => :get}
   match '/runs.:format', :to => 'runs#create', :conditions => {:method => :post}
+  match "/up", :to => "bootstrap#up", :conditions => {:method => :get}, :format => :sh
 end
