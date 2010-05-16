f4a89282880a2264ba8290b0ac0f3bb4e17b0af8
Ben Hoskings <ben@hoskings.net>
2010-05-16 23:36:10 +1000

    Added babushka.me/hard, which runs like babushka.me/up but accepting all defaults.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 BabushkaMe::Application.routes.draw do |map|
   match '/deps/search.:format/:q', :to => 'deps#search', :conditions => {:method => :get}
   match '/runs.:format', :to => 'runs#create', :conditions => {:method => :post}
-  match "/up", :to => "bootstrap#up", :conditions => {:method => :get}, :format => :sh
+  %w[up hard].each {|style|
+    match "/#{style}", :to => "bootstrap##{style}", :conditions => {:method => :get}, :format => :sh
+  }
 end
