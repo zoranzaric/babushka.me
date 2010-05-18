cfb2202bdb221d1fb3964147c4cea5d6bc135af2
Ben Hoskings <ben@hoskings.net>
2010-05-19 02:02:35 +1000

    Use a single url that accepts opts, instead of separate urls for every option. This also allows options to be combined.

--- app/controllers/bootstrap_controller.rb
+++ app/controllers/bootstrap_controller.rb
@@ -1,2 +1,9 @@
 class BootstrapController < ApplicationController
+  def up
+    opts = (params[:opts] || '').split(',').inject({}) {|hsh,opt|
+      hsh[opt.to_sym] = true
+      hsh
+    }
+    render :action => 'up', :layout => false, :locals => {:opts => opts}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 BabushkaMe::Application.routes.draw do |map|
   match '/deps/search.:format/:q', :to => 'deps#search', :conditions => {:method => :get}
   match '/runs.:format', :to => 'runs#create', :conditions => {:method => :post}
-  %w[up hard privately].each {|style|
-    match "/#{style}", :to => "bootstrap##{style}", :conditions => {:method => :get}, :format => :sh
-  }
+
+  match "/up", :to => "bootstrap#up", :conditions => {:method => :get}, :format => :sh
+  match "/up/:opts", :to => "bootstrap#up", :conditions => {:method => :get}, :format => :sh, :opts => /[a-z0-9\-_,]+/i
 end
