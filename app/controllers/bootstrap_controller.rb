f61cb6f75e7a09388a0256a63259246dc94bb48f
Ben Hoskings <ben@hoskings.net>
2010-05-19 02:08:21 +1000

    Respond with HTTP 400 when bad options are supplied.

--- app/controllers/bootstrap_controller.rb
+++ app/controllers/bootstrap_controller.rb
@@ -1,9 +1,13 @@
 class BootstrapController < ApplicationController
+  VALID_OPTS = %w[hard privately]
   def up
-    opts = (params[:opts] || '').split(',').inject({}) {|hsh,opt|
-      hsh[opt.to_sym] = true
-      hsh
-    }
-    render :action => 'up', :layout => false, :locals => {:opts => opts}
+    opt_list = (params[:opts] || '').split(',')
+    invalid_opts = opt_list.reject {|opt| VALID_OPTS.include? opt }
+    if !invalid_opts.empty?
+      render :status => 400, :text => "The options #{invalid_opts.inspect} aren't recognised. The valid ones are #{VALID_OPTS.inspect}."
+    else
+      opts = Hash[*opt_list.map {|i| [i.to_sym, true] }.flatten]
+      render :action => 'up', :layout => false, :locals => {:opts => opts}
+    end
   end
 end
