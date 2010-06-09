518552dd1451489ee6751e8213f23b08a0089b08
Ben Hoskings <ben@hoskings.net>
2010-06-10 00:59:45 +1000

    Added babushka#tarball action, that queues a babushka run to bring the tarball up to date via delayed_job.

---
+++ app/helpers/babushka_helper.rb
@@ -0,0 +1,2 @@
+module BabushkaHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ BabushkaMe::Application.routes.draw do |map|
   match '/deps/search.:format/:q', :to => 'deps#search', :via => :get
   match '/runs.:format', :to => 'runs#create', :via => :post
 
+  match '/a_new_tarball', :to => 'babushka#tarball', :via => :post
+
   match "/up", :to => "bootstrap#up", :via => :get, :format => :sh
   match "/up/:opts", :to => "bootstrap#up", :via => :get, :format => :sh
 end
