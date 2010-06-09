87eb574a21b733a01fa50e7eb59f722f24ba4eda
Ben Hoskings <ben@hoskings.net>
2010-06-10 00:56:17 +1000

    Update :conditions => {:method => blah} to :via => blah in routes.rb, for rails 3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 BabushkaMe::Application.routes.draw do |map|
-  match '/deps/search.:format/:q', :to => 'deps#search', :conditions => {:method => :get}
-  match '/runs.:format', :to => 'runs#create', :conditions => {:method => :post}
+  match '/deps/search.:format/:q', :to => 'deps#search', :via => :get
+  match '/runs.:format', :to => 'runs#create', :via => :post
 
-  match "/up", :to => "bootstrap#up", :conditions => {:method => :get}, :format => :sh
-  match "/up/:opts", :to => "bootstrap#up", :conditions => {:method => :get}, :format => :sh
+  match "/up", :to => "bootstrap#up", :via => :get, :format => :sh
+  match "/up/:opts", :to => "bootstrap#up", :via => :get, :format => :sh
 end
